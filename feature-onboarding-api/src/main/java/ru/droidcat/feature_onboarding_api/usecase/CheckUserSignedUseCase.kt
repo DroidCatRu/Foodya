36972a8372f8f54db0c56865cb5669e8db8d5655
Artem Koshkin <artko2050@gmail.com>
2022-09-21 17:50:57 +0300

    Fixed check user signed use case

--- feature-onboarding-api/src/main/java/ru/droidcat/feature_onboarding_api/usecase/CheckUserSignedUseCase.kt
+++ feature-onboarding-api/src/main/java/ru/droidcat/feature_onboarding_api/usecase/CheckUserSignedUseCase.kt
@@ -1,11 +1,9 @@
 package ru.droidcat.feature_onboarding_api.usecase
 
 import ru.droidcat.core_db_api.DatabaseRepository
-import ru.droidcat.core_network_api.NetworkRepository
 
 class CheckUserSignedUseCase(
-    private val dbRepository: DatabaseRepository,
-    private val networkRepository: NetworkRepository
+    private val dbRepository: DatabaseRepository
 ) {
     suspend operator fun invoke(): Boolean {
         return !dbRepository.getUserDatabaseId().isNullOrBlank()
